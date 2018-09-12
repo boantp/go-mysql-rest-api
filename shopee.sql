e7bdefc803b39376c4b1a6620e585f70020cc7e9
Boan Pasaribu <boantuapasaribu@gmail.com>
2018-09-12 15:34:00 +0700

    new db

--- shopee.sql
+++ shopee.sql
@@ -1,27 +1,29 @@
-/*
-SQLyog Ultimate v10.42 
-MySQL - 5.5.5-10.1.28-MariaDB : Database - shopee
-*********************************************************************
-*/
+-- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
+--
+-- Host: localhost    Database: shopee
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.1.33-MariaDB
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`shopee` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
-USE `shopee`;
-
-/*Table structure for table `order_details` */
+--
+-- Table structure for table `order_details`
+--
 
 DROP TABLE IF EXISTS `order_details`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `order_details` (
-  `order_details_id` bigint(20) NOT NULL,
+  `order_details_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `orders_id` int(11) NOT NULL,
   `product_name` varchar(255) DEFAULT NULL,
   `tax_code_id` int(11) DEFAULT NULL,
@@ -32,17 +34,28 @@ CREATE TABLE `order_details` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `store_id` int(11) NOT NULL,
-  PRIMARY KEY (`order_details_id`),
-  KEY `fk_order_item_order_header_1` (`orders_id`),
-  CONSTRAINT `fk_order_item_order_header_1` FOREIGN KEY (`orders_id`) REFERENCES `orders` (`orders_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='number 1';
+  `tax_name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`order_details_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 COMMENT='number 1';
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Data for the table `order_details` */
+--
+-- Dumping data for table `order_details`
+--
 
-/*Table structure for table `orders` */
+LOCK TABLES `order_details` WRITE;
+/*!40000 ALTER TABLE `order_details` DISABLE KEYS */;
+INSERT INTO `order_details` VALUES (1,0,'Big Mac',1,1000.00,100.00,1100.00,0,'2018-09-11 19:54:20','2018-09-11 19:54:20',1,'Food'),(2,0,'Malboro',2,2000.00,50.00,2050.00,0,'2018-09-11 19:57:43','2018-09-11 19:57:43',1,'Tobacco'),(3,0,'Movie',3,150.00,0.50,150.50,0,'2018-09-11 19:58:54','2018-09-11 19:58:54',1,'Entertainment');
+/*!40000 ALTER TABLE `order_details` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
 
 DROP TABLE IF EXISTS `orders`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `orders` (
   `orders_id` int(11) NOT NULL,
   `store_id` int(11) DEFAULT NULL,
@@ -56,28 +69,49 @@ CREATE TABLE `orders` (
   KEY `fk_orders_store_1` (`store_id`),
   CONSTRAINT `fk_orders_store_1` FOREIGN KEY (`store_id`) REFERENCES `store` (`store_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Data for the table `orders` */
+--
+-- Dumping data for table `orders`
+--
 
-/*Table structure for table `store` */
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `store`
+--
 
 DROP TABLE IF EXISTS `store`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `store` (
   `store_id` int(11) NOT NULL,
   `store_name` varchar(50) DEFAULT NULL,
   `status` smallint(1) DEFAULT NULL,
   PRIMARY KEY (`store_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Data for the table `store` */
+--
+-- Dumping data for table `store`
+--
 
-insert  into `store`(`store_id`,`store_name`,`status`) values (1,'Grand Indonesia',1),(2,'Plaza Indonesia',1);
+LOCK TABLES `store` WRITE;
+/*!40000 ALTER TABLE `store` DISABLE KEYS */;
+INSERT INTO `store` VALUES (1,'Grand Indonesia',1),(2,'Plaza Indonesia',1);
+/*!40000 ALTER TABLE `store` ENABLE KEYS */;
+UNLOCK TABLES;
 
-/*Table structure for table `tax_code` */
+--
+-- Table structure for table `tax_code`
+--
 
 DROP TABLE IF EXISTS `tax_code`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tax_code` (
   `tax_code_id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
@@ -86,12 +120,25 @@ CREATE TABLE `tax_code` (
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`tax_code_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Data for the table `tax_code` */
+--
+-- Dumping data for table `tax_code`
+--
 
-insert  into `tax_code`(`tax_code_id`,`name`,`status`,`created_at`,`updated_at`) values (1,'food',1,'2018-09-10 01:36:16','2018-09-10 01:36:16'),(2,'tobacco',1,'2018-09-10 01:36:17','2018-09-10 01:36:17'),(3,'entertainment',1,'2018-09-10 01:36:20','2018-09-10 01:36:20');
+LOCK TABLES `tax_code` WRITE;
+/*!40000 ALTER TABLE `tax_code` DISABLE KEYS */;
+INSERT INTO `tax_code` VALUES (1,'food',1,'2018-09-09 18:36:16','2018-09-09 18:36:16'),(2,'tobacco',1,'2018-09-09 18:36:17','2018-09-09 18:36:17'),(3,'entertainment',1,'2018-09-09 18:36:20','2018-09-09 18:36:20');
+/*!40000 ALTER TABLE `tax_code` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-09-12 15:33:07
