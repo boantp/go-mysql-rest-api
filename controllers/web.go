788deea6a0c79e99a73e5a1769b32e9c297f6ad8
Boan Pasaribu <boantuapasaribu@gmail.com>
2018-09-14 03:50:21 +0700

    web controller

---
+++ controllers/web.go
@@ -0,0 +1,136 @@
+package controllers
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/boantp/go-mysql-rest-api/config"
+	"github.com/boantp/go-mysql-rest-api/models"
+	"github.com/julienschmidt/httprouter"
+)
+
+type (
+	// TaxCodeController represents the controller for operating on the Tax Code resource
+	WebController struct{}
+)
+
+func NewWebController() *WebController {
+	return &WebController{}
+}
+
+func (web WebController) Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	//get tax code
+	url := "http://localhost:3000/tax_code"
+	body := RequestGet(url)
+
+	tax := TaxCodeResp{}
+	jsonErr := json.Unmarshal(body, &tax)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+	taxData := tax.Data
+
+	config.TPL.ExecuteTemplate(w, "create.gohtml", taxData)
+}
+
+func (web WebController) FrontCartProcess(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	// get form values
+	formStoreId := r.FormValue("store_id")
+	formAmount := r.FormValue("amount")
+	formTaxCodeId := r.FormValue("tax_code_id")
+	formProductName := r.FormValue("product_name")
+	// string to int
+	storeId, err := strconv.Atoi(formStoreId)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// string to int
+	taxCodeId, err := strconv.Atoi(formTaxCodeId)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// convert form values
+	f64, err := strconv.ParseFloat(formAmount, 32)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//store new tax object
+	addTax := models.Tax{}
+	addTax.StoreId = storeId
+	addTax.ProductName = formProductName
+	addTax.TaxCode = taxCodeId
+	addTax.Amount = float64(f64)
+
+	// Marshal provided interface into JSON structure
+	uj, _ := json.Marshal(addTax)
+
+	//POST localhost:3000/cart
+	url := "http://localhost:3000/cart"
+
+	//make post request
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(uj))
+	req.Header.Set("X-Custom-Header", "myvalue")
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	data := struct {
+		RespStatus string
+		RespBody   string
+	}{
+		resp.Status,
+		string(body),
+	}
+
+	config.TPL.ExecuteTemplate(w, "created.gohtml", data)
+}
+
+func (web WebController) ViewBill(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	//get order by store_id
+	url := "http://localhost:3000/order/1"
+	body := RequestGet(url)
+
+	order := OrderResp{}
+	jsonErr := json.Unmarshal(body, &order)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+
+	config.TPL.ExecuteTemplate(w, "orders.gohtml", order)
+}
+
+func RequestGet(url string) []byte {
+	spaceClient := http.Client{
+		Timeout: time.Second * 2, // Maximum of 2 secs
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("User-Agent", "tax-calculator")
+	res, getErr := spaceClient.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	return body
+}
--- main.go
+++ main.go
@@ -1,21 +1,11 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"strconv"
-	"time"
-	// Standard library packages
 
-	"github.com/boantp/go-mysql-rest-api/config"
 	"github.com/boantp/go-mysql-rest-api/controllers"
-	"github.com/boantp/go-mysql-rest-api/models"
 	"github.com/julienschmidt/httprouter"
-	// Third party packages
 )
 
 func main() {
@@ -25,7 +15,9 @@ func main() {
 	tc := controllers.NewTaxCodeController()
 	cc := controllers.NewCartController()
 	oc := controllers.NewOrderController()
+	web := controllers.NewWebController()
 
+	/** REST API **/
 	// Get a tax code resource
 	router.GET("/tax_code", tc.GetTaxCode)
 	//POST object tax into cart or order_details
@@ -33,107 +25,10 @@ func main() {
 	//GET my bill with tax calculation result from order_details
 	router.GET("/order/:store_id", oc.GetMyBill)
 
-	//Frontend
-	router.GET("/", Index)
-	router.POST("/front/cart/process", FrontCartProcess)
-	router.GET("/order_view/:store_id", ViewBill)
+	/** Web Frontend **/
+	router.GET("/", web.Index)
+	router.POST("/front/cart/process", web.FrontCartProcess)
+	router.GET("/order_view/:store_id", web.ViewBill)
 
 	log.Fatal(http.ListenAndServe(":3000", router))
-}
-
-func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	//get tax
-	url := "http://localhost:3000/tax_code"
-	spaceClient := http.Client{
-		Timeout: time.Second * 2, // Maximum of 2 secs
-	}
-
-	req, err := http.NewRequest(http.MethodGet, url, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	req.Header.Set("User-Agent", "tax-calculator")
-	res, getErr := spaceClient.Do(req)
-	if getErr != nil {
-		log.Fatal(getErr)
-	}
-
-	body, readErr := ioutil.ReadAll(res.Body)
-	if readErr != nil {
-		log.Fatal(readErr)
-	}
-
-	tax := TaxCodeResp{}
-	jsonErr := json.Unmarshal(body, &tax)
-	if jsonErr != nil {
-		log.Fatal(jsonErr)
-	}
-	taxData := tax.Data
-	config.TPL.ExecuteTemplate(w, "create.gohtml", taxData)
-}
-
-type TaxCodeResp struct {
-	RespCode string           `json:"response_code"`
-	RespDesc string           `json:"response_description"`
-	Data     []models.TaxCode `json:"data"`
-}
-
-type TaxCode struct {
-	TaxCodeId int    `json:"tax_code_id"`
-	Name      string `json:"name"`
-}
-
-type Tax struct {
-	StoreId     int     `json:"store_id"`
-	ProductName string  `json:"product_name"`
-	TaxCode     int     `json:"tax_code"`
-	TaxName     string  `json:"tax_name"`
-	Amount      float64 `json:"amount"`
-	TaxAmount   float64 `json:"tax_amount"`
-	TotalAmount float64 `json:"total_amount_item"`
-}
-
-func FrontCartProcess(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	// get form values
-	addTax := Tax{}
-	s := r.FormValue("store_id")
-	a := r.FormValue("amount")
-	t := r.FormValue("tax_code_id")
-	f64, err := strconv.ParseFloat(a, 32)
-	i, err := strconv.Atoi(s)
-	//j, err := strconv.Atoi(a)
-	k, err := strconv.Atoi(t)
-	addTax.StoreId = i
-	addTax.ProductName = r.FormValue("product_name")
-	addTax.Amount = float64(f64)
-	addTax.TaxCode = k
-	uj, _ := json.Marshal(addTax)
-
-	//POST localhost:3000/cart
-	url := "http://localhost:3000/cart"
-	//fmt.Println("URL:>", url)
-
-	//var jsonStr = []byte(`{"title":"Buy cheese and bread for breakfast."}`)
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(uj))
-	req.Header.Set("X-Custom-Header", "myvalue")
-	req.Header.Set("Content-Type", "application/json")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	// fmt.Println("response Status:", resp.Status)
-	// fmt.Println("response Headers:", resp.Header)
-	// body, _ := ioutil.ReadAll(resp.Body)
-	// fmt.Println("response Body:", string(body))
-
-	config.TPL.ExecuteTemplate(w, "created.gohtml", resp)
-}
-
-func ViewBill(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
 }
